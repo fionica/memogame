a07b4a7bb576811a7b5d62ab94cd0ba3385107a4
fionica <fionica@fionica>
2018-12-26 02:13:15 +0300

    функция process() теперь возвращает словарь, где ключи - индексы, значения - пути до файлов. Добавлен класс CardLabel(QLabel).

--- memogame.py
+++ memogame.py
@@ -5,103 +5,110 @@ from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
 from PyQt5.QtGui import QPixmap
 
-n = 4 # Сторона квадрата
-size = 120 # Сторона карты
-#---------------------------------------------
-class Game(QMainWindow):
+N = 4 # Сторона квадрата
+SIZE = 150 # Сторона карты
+#---------------------------------------------------------------------
+# Класс для создания карты
+class CardLabel(QLabel):
+
+    card_is_open = False # True - карточка открыта
+    img_path = '' # Индивидуальная картинка карты (путь к файлу)
+    card_index = -1 # По умолчанию, индекс карточки
+    #timer = 0
+    
+    
+    def __init__(self, parent):
+        super().__init__(parent)        
+    
+    
+    # Обработчик таймера:
+    def tick(self):
+        #self.setPixmap(QPixmap("Colours/Win.jpg"))
+        self.setPixmap(QPixmap("Colours/Back.jpg"))
+        self.timer.stop()
+        
+#---------------------------------------------------------------------
+# Основной класс
+class Game(QMainWindow):            
+    cards = []
+    
     def __init__(self):
         super().__init__()        
         self.initUI()
+           
     
     def initUI(self):
-        self.setGeometry(850, 850, 850, 850)
+        self.setGeometry(50, 50, 850, 850)
         self.setWindowTitle('MEMO')
-        
-        cards = Process()
-        print(cards)
+                
+        ## Для вычисления координат -----
         start_coord = 60
         coord_x = start_coord
         coord_y = start_coord
         between = 30
         cnt_x = 1
         cnt_y = 1
-        for card in cards:
-            for j in range(2):
-                name = 'label' + card[j]
-                self.name = QLabel(self)
-                pixmap = QPixmap("Colours/Back.jpg")
-                self.name.setPixmap(pixmap)
-                self.name.move(coord_x, coord_y)
-                #-------------------------------------------------------------------
-                # Определение coord_x:
-                if (cnt_x % n == 0):
-                    coord_x = start_coord
-                    #надо coord_y и pixmap отправить в event_filter
-                    cnt_x = 1
-                else:
-                    coord_x += size + between
-                    cnt_x += 1    
-                #-------------------------------------------------------------------
-                # Определение coord_y:
-                coord_y = start_coord + (cnt_y // n)*size + (cnt_y // n)*between
-                cnt_y += 1
-                #-------------------------------------------------------------------
-                self.name.resize(size, size)
-                
-                pixmap = QPixmap(card)
-                self.name.installEventFilter(self)
-                self.timer = QTimer()
-                self.timer.timeout.connect(self.tick)                
-        #---------------------------------------
-        '''
-        # Для каждого объекта Label
-        self.label = QLabel(self)
-        pixmap = QPixmap("Colours/Back.jpg")
-        self.label.setPixmap(pixmap)
-        self.label.move(60, 60)
-        self.label.resize(160, 160)
-
-        self.label.installEventFilter(self)
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.tick)
-        '''
-        #---------------------------------------
+        ##-------------------------------           
         
+        card_images = process()
+        for i in range(N**2):            
+            self.card = CardLabel(self)
+            self.card.img_path = card_images[i]
+            self.card.setPixmap(QPixmap("Colours/Back.jpg"))
+            self.card.setGeometry(coord_x, coord_y, SIZE, SIZE)            
+            self.card.installEventFilter(self)
+            self.card.card_is_open = False
+            self.card.card_index = i
+            self.card.timer = QTimer()
+            self.card.timer.timeout.connect(self.card.tick)
+            
+            self.cards.append(self.card)
+            
+            ##--------------------------------------------------
+            ## Определение coord_x:
+            if (cnt_x % N == 0):
+                coord_x = start_coord
+                cnt_x = 1
+            else:
+                coord_x += SIZE + between
+                cnt_x += 1    
+            ##--------------------------------------------------
+            ## Определение coord_y:
+            coord_y = start_coord + (cnt_y // N)*SIZE + (cnt_y // N)*between
+            cnt_y += 1
+            ##--------------------------------------------------      
+
+
     #http://www.cyberforum.ru/python-graphics/thread2361731.html
     def eventFilter(self, obj, event):
         if event.type() == 2:
-            btn = event.button()
-            pixmap = QPixmap("Bern.jpg")
-            
-            if btn == 1: 
-                self.label.setPixmap(pixmap)
-                self.timer.start(2000)
-            
-        return super(QMainWindow, self).eventFilter(obj, event)        
-   
-    def tick(self):
-        pixmap_win = QPixmap("Colours/Win.jpg")
-        self.label.setPixmap(pixmap_win)
-        self.timer.stop()        
-        
-#---------------------------------------------
-def Process():
-    files = ['Colours/Yellow.jpg', 'Colours/Red.jpg',\
+            btn = event.button()            
+            if btn == 1:
+                obj.setPixmap(QPixmap(obj.img_path))
+                obj.timer.start(500)            
+        return super(QMainWindow, self).eventFilter(obj, event)
+    
+    
+#---------------------------------------------------------------------
+# process() произвольно сопоставляет индекс и путь до файла.
+# Возвращает словарь cards, где ключи - индексы от 0 до 15, 
+# значения - пути до файлов. 
+def process():
+    paths = ['Colours/Yellow.jpg', 'Colours/Red.jpg',\
              'Colours/Purple.jpg', 'Colours/Pink.jpg',\
              'Colours/Orange.jpg', 'Colours/Lilac.jpg',\
              'Colours/LGreen.jpg', 'Colours/Blue.jpg']
-    labels = [s.zfill(2) for s in list(map(str, list(range(1, 17))))]
+    
+    paths += paths
     
     cards = {}
-    for file in files:
-        current_label1 = random.choice(labels)
-        del labels[labels.index(current_label1)]
-        current_label2 = random.choice(labels)
-        del labels[labels.index(current_label2)]        
-        cards[file] = (current_label1, current_label2)
+    for i in range(N**2):
+        current_path = random.choice(paths)
+        del paths[paths.index(current_path)]      
+        cards[i] = current_path
         
-    return cards        
-#---------------------------------------------
+    return cards 
+#---------------------------------------------------------------------
 if __name__ == '__main__':
     app = QApplication(sys.argv)    
     ex = Game()
