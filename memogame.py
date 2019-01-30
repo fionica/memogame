3ef1f3a7d9e03443c35e71eabc739de9d37d3716
fionica <fionica@fionica>
2019-01-31 02:27:05 +0300

    Пользователь может изменить набор картинок в рпоцессе игры.

--- memogame.py
+++ memogame.py
@@ -5,11 +5,12 @@ import sys # Для cmd
 import random # Для определения местоположения загруженных изображений на картах
 import winsound # Для озвучки переворота карты
 
+from PIL import Image
 from PyQt5 import uic
 from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow,\
      QLabel, QPushButton, QButtonGroup, QRadioButton
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QImage
 
 N = 4 # Сторона квадрата
 SIZE = 150 # Сторона карты
@@ -20,10 +21,9 @@ class CardLabel(QLabel):
 
     card_is_guessed = False # True - карточка открыта (отгадана)
     img_path = '' # Индивидуальная картинка карты (путь к файлу)
-    back_img_path = ''
-    card_index = -1 # По умолчанию, индекс карточки
+    card_index = -1 # индекс карточки по умолчанию
     
-    #Координаты карты:
+    # Координаты карты:
     x = 0
     y = 0
     
@@ -36,62 +36,122 @@ class CardLabel(QLabel):
     #---
     # Обработчик таймера:
     def tick(self):
-        self.setPixmap(QPixmap(self.back_img_path)) # Закрываем карту
+        self.setPixmap(QPixmap(self.back_img_path)) # закрываем карту
         self.timer.stop()
+  
         
 #---------------------------------------------------------------------
 # Основной класс
 class Game(QMainWindow):            
     cards = []
-    first_open_card_index = -1 #индекс первой (в паре) открытой карты
+    first_open_card_index = -1 # индекс первой (в паре) открытой карты
     
     #---
     def __init__(self):
         super().__init__()        
         self.initUI()
-           
+        
+        
     #---
     def initUI(self):        
         self.setGeometry(50, 50, 820, 860)
         self.setWindowTitle('MEMO')
         uic.loadUi('uic.ui', self)
         
-        self.back_img_path = 'images/greenback.jpg' # по умолчанию
-        self.initCards()
+        # Формирование Label для RadioButton -------------------------------
+        ##---Набор 1:
+        img = GenerateImagesSet('images/cities/')
+        # Конвертируем PIL.Image в QImage:
+        data = img.tobytes("raw", "RGB")
+        q_img = QImage(data, img.size[0], img.size[1], QImage.Format_RGB888)        
+        # Отображаем набор картинок:
+        self.label_201.setPixmap(QPixmap.fromImage(q_img))
         
-        # Изменение цвета рубашки карты
-        #self.label = QLabel('Current')
+        ##---Набор 2:
+        img = GenerateImagesSet('images/colours/')
+        # Конвертируем PIL.Image в QImage:
+        data = img.tobytes("raw","RGB")
+        q_img = QImage(data, img.size[0], img.size[1], QImage.Format_RGB888)        
+        # Отображаем набор картинок:
+        self.label_202.setPixmap(QPixmap.fromImage(q_img))    
+        # ------------------------------------------------------------------
         
-        self.radio_button_data = {self.radioButton_101: 'images/pinkback.jpg',
-                                  self.radioButton_102: 'images/greenback.jpg'}
+        # Возможные наборы картинок
+        cities = ('images/cities/01.jpg',\
+                  'images/cities/02.jpg',\
+                  'images/cities/03.jpg',\
+                  'images/cities/04.jpg',\
+                  'images/cities/05.jpg',\
+                  'images/cities/06.jpg',\
+                  'images/cities/07.jpg',\
+                  'images/cities/08.jpg')
+    
+        colours = ('images/colours/01.jpg',\
+                   'images/colours/02.jpg',\
+                   'images/colours/03.jpg',\
+                   'images/colours/04.jpg',\
+                   'images/colours/05.jpg',\
+                   'images/colours/06.jpg',\
+                   'images/colours/07.jpg',\
+                   'images/colours/08.jpg')
+        
+        # путь картинки-рубашки по умолчанию
+        self.back_img_path = 'images/pinkback.jpg' 
+        
+        # набор картинок по умолчанию
+        self.images_set = cities 
+        
+        self.initCards() # инициализация карт
+        
+        # Изменение цвета рубашки карты   
+        # {объект RadioButton: путь до картинки-рубашки}
+        self.radio_button_data_01 =\
+        {self.radioButton_101: 'images/pinkback.jpg',
+         self.radioButton_102: 'images/greenback.jpg'} 
         
         self.button_group_01 = QButtonGroup()
         self.button_group_01.addButton(self.radioButton_101)
         self.button_group_01.addButton(self.radioButton_102)
         
-        self.radioButton_102.setChecked(True)
+        self.radioButton_101.setChecked(True) # устанавливаем "маркер списка"
 
-        self.button_group_01.buttonClicked.connect(self._on_radio_button_clicked)
-        #print('clicked')
-
-        #print(self.back_img_path)        
+        self.button_group_01.buttonClicked.connect(self.on_radio_button_01_clicked)
+        
+        # Изменение набора картинок
+        # {объект RadioButton: кортеж путей до картинок}
+        self.radio_button_data_02 = {self.radioButton_201: cities,
+                                     self.radioButton_202: colours}
+        
+        self.button_group_02 = QButtonGroup()
+        self.button_group_02.addButton(self.radioButton_201)
+        self.button_group_02.addButton(self.radioButton_202)
+    
+        self.radioButton_201.setChecked(True) # устанавливаем "маркер списка"
+    
+        self.button_group_02.buttonClicked.connect(self.on_radio_button_02_clicked)    
         
             
      
     #---        
-    def _on_radio_button_clicked(self, button):
-        #print(button)   
-        self.back_img_path = self.radio_button_data[button]
+    def on_radio_button_01_clicked(self, button):   
+        self.back_img_path = self.radio_button_data_01[button]
+        
         for i in range(N**2):
-            if (self.cards[i].card_is_guessed == False):
-                self.cards[i].back_img_path = self.back_img_path
-                self.cards[i].setPixmap(QPixmap(self.back_img_path))
-        print('clicked')
-        #print(self.back_img_path)
+            if (self.cards[i].card_is_guessed == False): # если карта не отгадана
+                self.cards[i].back_img_path = self.back_img_path # мы меняем рубашку
+                self.cards[i].setPixmap(QPixmap(self.back_img_path)) # и устанавливаем её на карту
         
         
+    #---        
+    def on_radio_button_02_clicked(self, button):
+        self.images_set = self.radio_button_data_02[button]
+        
+        self.resetGame() # перезапускаем игру с новым images_set
+        
+        
+    #---    
+    # Формирование игрового пространства
     def initCards(self):
-        #--- Формирование игрового пространства -----------------------------
         ## Для вычисления координат -----
         start_coord = 60
         coord_x = start_coord
@@ -103,7 +163,7 @@ class Game(QMainWindow):
         self.timerGameOver = QTimer()
         self.timerGameOver.timeout.connect(self.GameOverEvent)
 
-        self.card_images = process()
+        self.card_images = process(self.images_set)
         for i in range(N**2):            
             self.card = CardLabel(self, self.back_img_path)
             self.card.img_path = self.card_images[i]
@@ -135,6 +195,7 @@ class Game(QMainWindow):
             coord_y = start_coord + (cnt_y // N)*SIZE + (cnt_y // N)*between
             cnt_y += 1
             ##-------------------------------------------------- 
+        
         
     #---
     #http://www.cyberforum.ru/python-graphics/thread2361731.html
@@ -206,6 +267,7 @@ class Game(QMainWindow):
     
     
     #---
+    # Окончание игры
     def GameOverEvent(self):                                
         winsound.PlaySound('wav/gameover2.wav', winsound.SND_FILENAME)  
         self.resetGame()
@@ -213,33 +275,25 @@ class Game(QMainWindow):
         
         
     #---
-    # Закрыть все карты, начать игру
+    # Перезапуск игры
     def resetGame(self):
         #Генерируем новые карточки:
-        self.card_images = process()
+        self.card_images = process(self.images_set)
         
         for i in range(N**2):
             self.cards[i].card_is_guessed = False
             self.cards[i].back_img_path = self.back_img_path
             self.cards[i].setPixmap(QPixmap(self.back_img_path))
             self.cards[i].img_path = self.card_images[i]
+        
             
 #---------------------------------------------------------------------
 # process() произвольно сопоставляет индекс и путь до файла.
 # Возвращает словарь cards, где ключи - индексы от 0 до 15, 
 # значения - пути до файлов. 
-def process():
-    paths1 = ['images/cities/01.jpg', 'images/cities/05.jpg',\
-             'images/cities/02.jpg', 'images/cities/06.jpg',\
-             'images/cities/03.jpg', 'images/cities/07.jpg',\
-             'images/cities/04.jpg', 'images/cities/08.jpg']
+def process(images_set): 
     
-    paths2 = ['images/colours/01.jpg', 'images/colours/05.jpg',\
-              'images/colours/02.jpg', 'images/colours/06.jpg',\
-             'images/colours/03.jpg', 'images/colours/07.jpg',\
-             'images/colours/04.jpg', 'images/colours/08.jpg']    
-    
-    paths = paths1    
+    paths = list(images_set)
     paths += paths
     
     cards = {}
@@ -249,6 +303,31 @@ def process():
         cards[i] = current_path
 
     return cards 
+
+
+#---------------------------------------------------------------------
+# Создание картинки набора для Label с RadioButton
+def GenerateImagesSet(path):
+    w = 1280 # 200
+    h = 640 # 100
+    img = Image.new('RGB', (w, h))
+    x = 0
+    y = 0
+    for i in range(1, 9):
+        filename = '0' + str(i) + '.jpg'
+        img_tmp = Image.open(path + filename)
+        img.paste(img_tmp, (x, y))
+
+        if(i == 4):
+            x = 0        
+            y = y + 320
+        else:    
+            x = x + 320
+        
+    resized_img = img.resize((200, 100))   
+    return resized_img
+
+
 #---------------------------------------------------------------------
 if __name__ == '__main__':
     app = QApplication(sys.argv)    
